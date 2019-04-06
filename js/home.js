2975ab058c16b39406c70388454de258a2b664f3
Elliott <elliottcost@gmail.com>
2019-04-06 13:37:06 -0400

    _day sitewide

--- js/home.js
+++ js/home.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 
+    // Changes _day to field name
+    $('input[name=first_name]').keyup(function(){
+      $('.initname').text( this.value + ' ' );
+
+      var field = $('input[name=first_name]').val();
+
+      if (field == '') {
+        console.log('empty');
+        $('.initname').text( '_' );
+      }
+    });
+
+    // FAQ's accordian
     $('h3').click(function(){
       console.log('hello');
       $(this).next('p').toggle();
-    //   $('.bio-body').slideToggle();
       $(this).children('.symbol').toggleClass('closed');
     });
-  
+
   });
