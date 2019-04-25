1268b755c7482977174b719bde415caa77a0c9a4
admin <admin@admins-MacBook-Pro-2.local>
2019-04-25 15:25:07 -0400

    leaves

--- js/home.js
+++ js/home.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
 
-    // Changes _day to field name
-    $('input[name=first_name]').keyup(function(){
+    // Changes instances of '_day' to first name field
+    $('input[name=firstname]').keyup(function() {
       $('.initname').text( this.value + ' ' );
-      $('.first-name').text( this.value + ' ' );
+      $('.firstname').val( this.value );
 
-      var field = $('input[name=first_name]').val();
+      var field = $('input[name=firstname]').val();
 
       if (field == '') {
         console.log('empty');
         $('.initname').text( '_' );
       }
+    });
+
+    // Populates the website with leaves
+    names = ['Elliott', 'Laurel', 'Ralf', 'Damian', 'Josh', 'Lily', 'Mary Helena', 'Michael', 'George', 'Toby', 'Mary', 'Andrew', 'Diana', 'Sara', 'Becca', 'Adam', 'Scott', 'Morgan', 'Sean', 'Doris', 'Joel', 'Nathan', 'Dennis', 'Adriana', 'Julia', 'James', 'Marc', 'Penny', 'Sprout', 'Tara', 'Brandon', 'Cole', 'Hannah', 'Willa', 'Cameron', 'Luke', 'Dimitra', 'Robert', 'Colin', 'Dan', 'Pirijan', 'Lana', 'Wilson', 'Fred', 'Haegen', 'Yanny', 'John', 'Linda', 'Perry', 'Jose', 'Carlos', 'Rebecca', 'Rynda', 'Max', 'Sox', 'Mario', 'Zoe', 'Joseph', 'Kolton', 'Cedar', 'Mark', 'Harrison', 'Allison', 'Kait', 'Eaton'];
+
+    $.each(names, function(index, name) {
+      $('.names').append(`
+        <div class="leaf"><label>New York</label>${name}</div>
+      `);
     });
 
     // FAQ's accordian
@@ -20,4 +29,9 @@ $(document).ready(function(){
       $(this).children('.symbol').toggleClass('closed');
     });
 
-  });
+    // Fade out page button
+    $('.fadeout').click(function(){
+      $('body').fadeOut(2000);
+    });
+
+});
